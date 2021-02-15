7ae2294193be7b9a09181f79e070bb1dc792ccfb
DESKTOP-0UPI14J\hp <mmbalisi@csmbataan.ph.education>
2021-02-15 20:03:07 +0800

    Finished Application.

--- MarkBalisi.PlayingLocalVideoFile/MarkBalisi.PlayingLocalVideoFile/MainWindow.xaml.cs
+++ MarkBalisi.PlayingLocalVideoFile/MarkBalisi.PlayingLocalVideoFile/MainWindow.xaml.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Media;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Runtime.Serialization;
 
 namespace MarkBalisi.PlayingLocalVideoFile
 {
@@ -23,6 +26,57 @@ namespace MarkBalisi.PlayingLocalVideoFile
         public MainWindow()
         {
             InitializeComponent();
+            VideoControl.Volume = 100;
+
+            VideoControl.Width = 440;
+
+            VideoControl.Height = 280;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDlg = new OpenFileDialog();
+
+            openDlg.InitialDirectory = @"c:\";
+
+            openDlg.ShowDialog();
+
+            txtMediaPathTextbox.Text = openDlg.FileName;
+        }
+
+        private void btnPlayClicl_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtMediaPathTextbox.Text.Length <= 0)
+
+            {
+
+                MessageBox.Show("Enter a valid media file");
+
+                return;
+
+            }
+            VideoControl.Source = new System.Uri(txtMediaPathTextbox.Text);
+
+            VideoControl.Play();
+
+        }
+
+        public class Uri : System.Runtime.Serialization.ISerializable
+        {
+            public void GetObjectData(SerializationInfo info, StreamingContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            VideoControl.Pause();
+        }
+
+        private void btnStopClick_Click(object sender, RoutedEventArgs e)
+        {
+            VideoControl.Stop();
         }
     }
 }
